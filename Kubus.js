eb42914a22a47565df0805555ea448240023b9f2
24 jauhar fadil <jauharfadil74@gmail.com>
2026-06-19 16:45:07 +0700

    Delete Kubus.js

--- Kubus.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-
-let s = Number(prompt("masukkan sisi kubus"));
-
-let volume = s * s * s;
-let luasPermukaan = 6 * s * s;
-
-console.log("Volume Kubus =", volume);
-console.log("Luas Permukaan Kubus =", luasPermukaan);
